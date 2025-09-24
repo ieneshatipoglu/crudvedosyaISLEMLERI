65d1a20ac05918f41d19817069423b58add9906d
ismail enes hatipoglu <ismaileneshatipoglu@gmail.com>
2025-09-24 11:13:53 +0300

    Controller ve View işlemini gerçekleştirdik

---
+++ crudvedosyaISLEMLERI/Controllers/SarkiciController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using crudvedosyaISLEMLERI.Models;
+using crudvedosyaISLEMLERI.Models.Baglanti;
+
+namespace crudvedosyaISLEMLERI.Controllers
+{
+    public class SarkiciController : Controller
+    {
+        private readonly DbBaglantisi _context;
+        public SarkiciController(DbBaglantisi context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var sarkilar=_context.Sarkilar.ToList();
+            return View(sarkilar);
+            
+        }
+    }
+}
