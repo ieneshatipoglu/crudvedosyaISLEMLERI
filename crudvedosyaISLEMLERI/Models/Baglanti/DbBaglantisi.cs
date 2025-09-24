986ed7f46ec3ebb05a9aa9f03745744c3dd9b55a
ismail enes hatipoglu <ismaileneshatipoglu@gmail.com>
2025-09-24 10:15:36 +0300

    Migration işlemi yapıldı

---
+++ crudvedosyaISLEMLERI/Models/Baglanti/DbBaglantisi.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using crudvedosyaISLEMLERI.Models;
+
+namespace crudvedosyaISLEMLERI.Models.Baglanti
+{
+    public class DbBaglantisi : DbContext
+    {
+        public DbBaglantisi(DbContextOptions<DbBaglantisi>options)
+            : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+        public DbSet<Sarki>Sarkilar {  get; set; }
+    }
+}
