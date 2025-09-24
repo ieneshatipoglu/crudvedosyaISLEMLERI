af42096b79d4cf5a3ffc3f0591e0b535a0b8f182
ismail enes hatipoglu <ismaileneshatipoglu@gmail.com>
2025-09-24 10:10:02 +0300

    Add project files.

---
+++ crudvedosyaISLEMLERI/Models/Sarki.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using crudvedosyaISLEMLERI.Models;
+
+namespace crudvedosyaISLEMLERI.Models
+{
+    public class Sarki
+    {
+        public int SarkiId { get; set; }
+        public string SarkiAdi { get; set; }
+        public string SarkicininAdi { get; set; }
+    }
+}
