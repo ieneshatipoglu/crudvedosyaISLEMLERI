fcd0d7e7e409ca242b5fcc1913c94f5f2b7ea8f1
ismail enes hatipoglu <ismaileneshatipoglu@gmail.com>
2025-09-25 10:25:31 +0300

    sınıfa rol yapısı da ekledik-admin ve kullanıcıyı ayırt etmek için

---
+++ crudvedosyaISLEMLERI/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using crudvedosyaISLEMLERI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MerleTur.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Login login)
+        {
+            if (login.KullaniciAdi == "admin" && login.Sifre == "admin")
+            {
+                // Giriş başarılı -> Ana sayfaya yönlendir
+                return RedirectToAction("Index", "Sarkici");
+            }
+             
+
+            // Hata varsa ViewBag ile uyarı gönder
+            ViewBag.Hata = "Kullanıcı adı veya şifre hatalı!";
+            return View();
+        }
+    }
+}
